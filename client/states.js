0ab8df68869c0aea95a15b28dd867794ff70dc1c
Francisco Aviles <francisco.aviles@flex.com>
2017-09-07 13:33:36 -0700

    [Studio] Add screen "survey"

--- client/states.js
+++ client/states.js
@@ -60,5 +60,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.survey', {
+        views: {
+            app: {
+                controller: 'app_survey',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.survey');
+                }
+            }
+        }
     });
 });
