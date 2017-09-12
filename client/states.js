765ff98ed3a4d158dc1df6275fc30977aabfd825
Francisco Aviles <francisco.aviles@flex.com>
2017-09-12 08:16:10 -0700

    [Studio] Add screen "logintest"

--- client/states.js
+++ client/states.js
@@ -69,5 +69,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.logintest', {
+        views: {
+            app: {
+                controller: 'app_logintest',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.logintest');
+                }
+            }
+        }
     });
 });
